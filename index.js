c35487d26aa0b040f37cf6497356598db280e70c
suju4021 <2023430063@vpmthane.org>
2025-01-09 20:44:04 +0530

    passwordhashed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express();
 const { userRouter } = require("./routes/user")
 const { courseRouter } = require("./routes/course")
@@ -8,4 +9,10 @@ app.use("api/v1/user", userRouter)
 app.use("api/v1/course", courseRouter)
 app.use("api/v1/admin", adminRouter)
 
-app.listen(3000)
+
+async function main(){
+    await mongoose.connect("mongodb://localhost:27017/course-selling-app")
+    app.listen(3000)
+    console.log("Listening on port 3000")
+}
+main();
--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,47 @@
 const { Router } = require("express")
-const  userRouter = Router();
-const {UserModel} = require("../db")
-userRouter.post("/signup", (req, res) => {
+const bcrypt = require("bcrypt")
+const userRouter = Router();
+const { UserModel } = require("../db")
+const z = require("zod");
+const { mongoose } = require("mongoose");
 
+
+userRouter.post("/signup", async (req, res) => {
+    const { email, password, firstname, lastname } = req.body
+    // password hashing 
+    const salt = await bcrypt.genSalt(5)
+    const hashedpassword = await bcrypt.hash(password, salt)
+
+    // zod input schema
+    const user = z.object({
+        email: z.string().min(4).max(15),
+        hashedpassword: z.string().min(3).max(100)
+    })
+    try {
+        await UserModel.create({
+            firstname: firstname,
+            lastname: lastname,
+            email: email,
+            hashedpassword: hashedpassword
+        })
+        res.status(201).json({ message: "User created successfully" });
+
+    }
+    catch (e) {
+        res.json({
+            error: e.message
+        })
+    }
 })
 userRouter.post("/signin", (req, res) => {
 
 })
 
 userRouter.get("/purchases", (req, res) => {
-res.json({
-    message:"hello"
-})
+    res.json({
+        message: "hello"
+    })
 })
 module.exports = {
-    userRouter:userRouter
+    userRouter: userRouter
 }
